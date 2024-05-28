1ef19e0bf30b082a8990e93979d74d75fe1d3578
macshop <tml.murod@mail.ru>
2024-05-28 21:45:36 +0500

    edited

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from blog.views import NewestPostsListAPIView, MostViewedPostsListAPIView, \
-    WeeklyPopularPostsListAPIView, MonthlyPopularPostsListAPIView
+from blog import views
 
 urlpatterns = [
-    path('', NewestPostsListAPIView.as_view()),
-    path('most_viewed/', MostViewedPostsListAPIView.as_view()),
-    path('weekly/', WeeklyPopularPostsListAPIView.as_view()),
-    path('monthly/', MonthlyPopularPostsListAPIView.as_view())
+    path('', views.NewestPostsListAPIView.as_view()),
+    path('most_viewed/', views.MostViewedPostsListAPIView.as_view()),
+    path('weekly/',views.WeeklyPopularPostsListAPIView.as_view()),
+    path('monthly/', views.MonthlyPopularPostsListAPIView.as_view()),
+    path('recommended/', views.RecommendedPostsListAPIView.as_view()),
+    path('create_post/', views.PostCreateAPIView.as_view()),
 ]
